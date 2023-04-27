397e110fa48b93138eba5295d8a2438af1e7d018
alexandrabordianu <alexandradenisa.bordianu@yahoo.com>
2023-04-27 22:48:28 +0300

    Updated register page

--- register.php
+++ register.php
@@ -1,60 +1,57 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Register</title>
-  <link rel="stylesheet" type="text/css" href="mainstyle.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- stylesheet-uri folosite -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Slab">
+    <link rel="stylesheet" href="style.css">
+    <title>Inregistrare</title>
 </head>
 <body>
-  <div class="header">
-  	<h2>Register</h2>
-  </div>
-	
-  <form method="post" action="register.php">
-  	<?php include('errors.php'); ?>
-	
-  	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
-  	</div>
-	
-	<div class="input-group">
-  	  <label>Name</label>
-  	  <input type="text" name="name" value="<?php echo $name; ?>">
-  	</div>
-	
-	<div class="input-group">
-  	  <label>E-mail</label>
-  	  <input type="text" name="email" value="<?php echo $email; ?>">
-  	</div>
-	
-	<div class="input-group">
-  	  <label>Phone number</label>
-  	  <input type="text" name="phone" value="<?php echo $phone; ?>">
-  	</div>
-	
-	<div class="input-group">
-  	  <label>City</label>
-  	  <input type="text" name="city" value="<?php echo $city; ?>">
-  	</div>
-	
-  	<div class="input-group">
-  	  <label>Password</label>
-  	  <input type="password" name="password_1">
-  	</div>
-	
-  	<div class="input-group">
-  	  <label>Confirm password</label>
-  	  <input type="password" name="password_2">
-  	</div>
-	
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_user">Register</button>
-  	</div>
-	
-  	<p>
-  		Click here to sign in instead: <a href="login.php">Sign in</a>
-  	</p>
-  </form>
+    <!-- Formular inregistrare cont -->
+    <div id="banner">
+        <h3>Creeaza un cont</h3>
+        <form method="post" action="register.php">
+            <?php include('errors.php'); ?>
+            <div class="input-group">
+                <input type="text" name="username" placeholder="Nume de utilizator" value="<?php echo $username; ?>">
+            </div>
+            <br>
+            <div class="input-group">
+                <input type="text" name="name" placeholder="Nume si prenume" value="<?php echo $name; ?>">
+            </div>
+            <br>
+            <div class="input-group">
+                <input type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>">
+            </div>
+            <br>
+            <div class="input-group">
+                <input type="text" name="phone" placeholder="Numar de telefon" value="<?php echo $phone; ?>">
+            </div>
+            <br>
+            <div class="input-group">
+                <input type="text" name="city" placeholder="Judet" value="<?php echo $city; ?>">
+            </div>
+            <br>
+            <div class="input-group">
+                <input type="password" placeholder="Parola" name="password_1">
+            </div>
+            <br>
+            <div class="input-group">
+                <input type="password" placeholder="Reintrodu parola" name="password_2">
+            </div>
+            <br>
+            <div class="input-group">
+                <button type="submit" class="btn-readmore" name="reg_user">Inregistrare</button>
+            </div>
+            <br>
+            <a href="login.php" class="btn-readmore">Aveti deja un cont?</a>
+            <a href="welcome.php" class="btn-readmore">Inapoi</a>
+        </form>
+    </div>
 </body>
 </html>
